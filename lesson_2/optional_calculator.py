fd859f948b493efc69b58bfda573afb64f67d569
Ian Constantinos <ianconstan@gmail.com>
2025-03-04 17:10:28 -0500

    made 1 and 4 changes to optional_calculator

--- lesson_2/optional_calculator.py
+++ lesson_2/optional_calculator.py
@@ -3,43 +3,54 @@ def prompt(message):
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
 
     return False
+while True:
 
-prompt('Welcome to Calculator!')
+    prompt('Welcome to Calculator!')
 
-prompt("What's the first number?")
-number1 = input()
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+    prompt("What's the first number?")
     number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number1 = input()
 
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    prompt("What's the second number?")
     number2 = input()
 
-prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
-operation = input()
+    while invalid_number(number2):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number2 = input()
 
-while operation not in ["1", "2", "3", "4"]:
-    prompt("You must choose 1, 2, 3, or 4")
+    prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
     operation = input()
 
-match operation:
-    case "1":
-        output = int(number1) + int(number2)
-    case "2":
-        output = int(number1) - int(number2)
-    case "3":
-        output = int(number1) * int(number2)
-    case "4":
-        output = int(number1) / int(number2)
+    while operation not in ["1", "2", "3", "4"]:
+        prompt("You must choose 1, 2, 3, or 4")
+        operation = input()
 
-prompt(f"The result is {output}")
+    match operation:
+        case "1":
+            output = float(number1) + float(number2)
+        case "2":
+            output = float(number1) - float(number2)
+        case "3":
+            output = float(number1) * float(number2)
+        case "4":
+            output = float(number1) / float(number2)
+
+    prompt(f"The result is {output:.2f}")
+    cont = input('Would you like to perform more calculations? (Y/N)')
+    while True:
+        if cont == 'y'.casefold():
+            break
+        elif cont == 'n'.casefold():
+            print('Have a great day!')
+            quit()
+        else:
+            print('not a valid reply! Continue calculating? (y/n)')
+            cont = input()
